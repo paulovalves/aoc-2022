fccdd7f9a77f658588b4da199cbf47d8bf45e4d9
paulovalves <paulovalves@outlook.com>
2023-12-08 19:04:25 -0300

    day two done

--- daytwo/daytwo.go
+++ daytwo/daytwo.go
@@ -7,14 +7,18 @@ import (
 	"strings"
 )
 
+// Player: A for Rock, B for Paper, and C for Scissors
+// Points: 1 for Rock, 2 for Paper, and 3 for Scissors
+// Player: X for Rock, Y for Paper, and Z for Scissors
+// outcome: 0 - lost, 3 - draw, 6 - win
+
 type Game struct {
 	ROCK     string
 	PAPER    string
 	SCISSORS string
 }
 
-func calculatePoints() {
-
+func calculatePoints() int {
 	fileName := "daytwo/input"
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -25,16 +29,53 @@ func calculatePoints() {
 
 	scanner := bufio.NewScanner(file)
 
+	sum := 0
 	for scanner.Scan() {
 		el := scanner.Text()
 		l, r := splitElement(el)
-		fmt.Printf("\n%s -> %s", l, r)
+		s, n := outcome(l, r)
+		sum += n
+		fmt.Printf("\n%s = %d", s, n)
 	}
 
 	/*for _, el := range arr {
 		l, r := splitElement(el)
 		fmt.Printf("\n%s -> %s", l, r)
 	}*/
+	return sum
+}
+
+func outcome(left string, right string) (string, int) {
+	// Rock | Rock
+	if left == "A" && right == "X" {
+		return "draw", 1 + 3
+		// rock | paper
+	} else if left == "A" && right == "Y" {
+		return "WON", 2 + 6
+		// rock | scissors
+	} else if left == "A" && right == "Z" {
+		return "Lost", 3 + 0
+		// paper | rock
+	} else if left == "B" && right == "X" {
+		return "Lost", 1 + 0
+		// paper | paper
+	} else if left == "B" && right == "Y" {
+		return "Draw", 2 + 3
+		// paper | scissors
+	} else if left == "B" && right == "Z" {
+		return "WON", 3 + 6
+		// scissors | rock
+	} else if left == "C" && right == "X" {
+		return "WON", 1 + 6
+		// scissors | paper
+	} else if left == "C" && right == "Y" {
+		return "Lost", 2 + 0
+		// scissors | scissors
+	} else if left == "C" && right == "Z" {
+		return "Draw", 3 + 3
+	} else {
+		return "WRONG", 0
+	}
 }
 
 func splitElement(str string) (string, string) {
@@ -45,5 +86,6 @@ func splitElement(str string) (string, string) {
 }
 
 func DayTwo() {
-	calculatePoints()
+	result := calculatePoints()
+	fmt.Printf("\n\nResult: %d\n", result)
 }
